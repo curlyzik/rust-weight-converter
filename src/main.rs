95c521f3adb07ee7106c11bbfd1631f07f81561c
Nzekwe Isaac Edidiong <curlyzik@gmail.com>
2022-03-28 00:16:12 +0100

    make comments

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use std::io;
 
 fn main() {
     println!("Enter Your Weight (kg):");
+    // make the input mutable
     let mut input = String::new();
+
+    // create a mutable reference to the input
     io::stdin().read_line(&mut input).unwrap();
 
+    // convert the value to float data type
     let weight: f32 = input.trim().parse().unwrap();
 
-    let mut mars_weight = calc_weight_mars(weight);
+    let mars_weight = calc_weight_mars(weight);
     println!("weight on mars {}kg", mars_weight);
 }
 
